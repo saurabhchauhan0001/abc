91619c8a17eeefe5ff0263f0eb8b84a253b681a0
Your Name <your-email@example.com>
2026-07-07 15:40:46 +0530

    practice

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+let app = express();
+app.use(express.json());
+
+dotenv.config();
+let PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+    return res.status(200).json({message:"hello saurabh how are you?"})
+})
+
+app.get("/health",(req,res)=>{
+    return res.status(200).json({message:"all is good"})
+})
+
+    app.listen(PORT,()=>{
+        console.log(`server is running on port ${PORT}`)
+    })
